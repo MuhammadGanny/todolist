53540b3c732be2879f9343587ba2cd2478a78ff8
muhammadganny <mohammadganny@gmail.com>
2023-07-26 13:37:58 +0500

    adding n deleting

--- src/TodoContext.js
+++ src/TodoContext.js
@@ -1,33 +1,31 @@
-import React, {createContext,useState} from "react"
+import React, { createContext, useState } from "react";
 
 const TodoContext = createContext();
 
-const TodoProvider = ({children}) => {
-    const [todos, setTodos] = useState([]);
+const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
 
-    const addTodos = (text) => {
-        setTodos([...todos,{id:Date.now(), text}]);
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text }]);
+  };
 
-    };
+  const updateTodo = (id, newText) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, text: newText } : todo
+    );
+    setTodos(updatedTodos);
+  };
 
-    const updateTodo = (id, newText) => {
-        const updatedTodos = todos.map((todo) =>
-          todo.id === id ? { ...todo, text: newText } : todo
-        );
-        setTodos(updatedTodos);
-      };
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
 
-      const deleteTodo = (id) => {
-        const updatedTodos = todos.filter((todo) => todo.id !== id);
-        setTodos(updatedTodos);
-      };
-
-      return (
-        <TodoContext.Provider value={{ todos, addTodo, updateTodo, deleteTodo }}>
-          {children}
-        </TodoContext.Provider>
-      );
-
-
+  return (
+    <TodoContext.Provider value={ {todos, addTodo, updateTodo, deleteTodo }}>
+      {children}
+    </TodoContext.Provider>
+  );
 };
-export{TodoContext, TodoProvider};
+
+export { TodoContext, TodoProvider };
